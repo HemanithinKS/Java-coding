5c67c67219c874e7eb514902a2c1979b54bb22b5
Hemanithin K S <hemanithinks2003@gmail.com>
2023-11-21 16:44:37 +0530

    1

--- Day wise/Day-1/CLass programs/only programs/palindrome.java
+++ Day wise/Day-1/CLass programs/only programs/palindrome.java
@@ -6,7 +6,7 @@ public class palindrome {
         Scanner s = new Scanner(System.in);
 
         System.out.print("Enter a number: ");
-        int n = s.nextInt();
+        int n= s.nextInt();
 
         temp = n;
         while (n > 0) {
